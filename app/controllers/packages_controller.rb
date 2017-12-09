0a7379938f3dd38ae925f60bda63b47e3410ab4c
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-09 11:52:09 -0600

    added package/:id route to package controller with logic to only show package to a logged in user, where the package belongs to that user

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -7,7 +7,18 @@ class PackagesController < ApplicationController
     else
       redirect '/'
     end
-
   end
 
+  get '/packages/:id' do
+    package = Package.find(params[:id])
+    if logged_in?
+      if package.user == current_user
+        erb :'/packages/show'
+      else
+        redirect '/packages'
+      end
+    else
+      redirect '/'
+    end
+  end
 end
