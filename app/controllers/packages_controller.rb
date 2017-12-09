e3fa49a3097446fcbfe0f81b32c5dd41502243b3
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-09 12:21:55 -0600

    added edit routes to packages controller with logic for showing edit page

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -21,4 +21,26 @@ class PackagesController < ApplicationController
       redirect '/'
     end
   end
+
+  get '/packages/:id/edit' do
+    @package = Package.find(params[:id])
+    if logged_in?
+      if @package.user == current_user
+        erb :'/packages/edit'
+      else
+        redirect '/packages'
+      end
+    else
+      redirect '/'
+    end
+  end
+
+  patch '/packages/:id' do
+    @package = Package.find(params[:id])
+    @package.update(params)
+    redirect "/packages/#{@package.id}"
+  end
+
+
+
 end
