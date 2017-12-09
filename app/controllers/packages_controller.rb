a69cc21046def8e6e600c26096ec1682ce5fc7d9
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-08 22:58:59 -0600

    added route for /packages and view index for packages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,4 +1,13 @@
 class PackagesController < ApplicationController
 
+  get '/packages' do
+    if logged_in?
+      current_user
+      erb :"packages/index"
+    else
+      redirect '/'
+    end
+
+  end
 
 end
