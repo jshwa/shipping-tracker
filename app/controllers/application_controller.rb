5b66bdc5edeb1511277ec2805553b28e417df67d
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-09 20:22:00 -0600

    redirect user to packages if they're already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    if logged_in?
+      redirect '/packages'
+    else
+      erb :index
+    end
   end
 
   helpers do
