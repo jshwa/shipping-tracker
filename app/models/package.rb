01084fe16de557ddd56c59e351792f1aaa60a59e
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-09 20:14:39 -0600

    added URLs for DHL, UPS and USPS

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,8 +4,14 @@ class Package < ActiveRecord::Base
 
   def tracking_url
     case self.shipping_co
-    when "Fedex"
-      "https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=#{self.tracking_no}&action=track"
+    when "FEDEX"
+      "https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=#{self.tracking_no}"
+    when "DHL"
+      "http://www.dhl.com/en/express/tracking.html?AWB=#{self.tracking_no}&brand=DHL"
+    when "UPS"
+      "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums;=#{self.tracking_no}"
+    when "USPS"
+      "https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=#{self.tracking_no}"
     else
       "#"
     end
