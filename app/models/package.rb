f5f2285a43fa59f2da586558ac9e43bd994c7f12
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-09 20:00:34 -0600

    added tracking_url method to return a url to the tracking website for a package, added the method to the package index

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,4 +2,12 @@ class Package < ActiveRecord::Base
   belongs_to :user
   belongs_to :sender
 
+  def tracking_url
+    case self.shipping_co
+    when "Fedex"
+      "https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=#{self.tracking_no}&action=track"
+    else
+      "#"
+    end
+  end
 end
