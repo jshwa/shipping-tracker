d726bacaaaac4b78267905188966ae7547b6caa9
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-10 12:04:44 -0600

    fixed random text that got inserted

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,7 +4,6 @@ class Package < ActiveRecord::Base
 
   def tracking_url
     case self.shipping_co
-<<<<<<< HEAD
     when "FedEx"
       "https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=#{self.tracking_no}"
     when "DHL"
@@ -13,10 +12,8 @@ class Package < ActiveRecord::Base
       "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums;=#{self.tracking_no}"
     when "USPS"
       "https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=#{self.tracking_no}"
-=======
     when "Fedex"
       "https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=#{self.tracking_no}&action=track"
->>>>>>> 1d29fa2e575e6441b49472550577016ffd41f551
     else
       "#"
     end
