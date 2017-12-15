ec398a033dc55b6c28052089712275009e5a50cd
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-14 18:00:42 -0600

    refactored if statements to prevent error when entering a package that does not exist, and to prevent logged out user patching a package

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,6 @@ class PackagesController < ApplicationController
 
   get '/packages' do
     if logged_in?
-      current_user
       erb :"packages/index"
     else
       redirect to '/'
@@ -11,7 +10,6 @@ class PackagesController < ApplicationController
 
   get '/packages/new' do
     if logged_in?
-      current_user
       @errors = []
       erb :"/packages/new"
     else
@@ -43,9 +41,8 @@ class PackagesController < ApplicationController
   end
 
   get '/packages/:id' do
-    @package = Package.find(params[:id])
     if logged_in?
-      if @package.user == current_user
+      if @package = current_user.packages.find_by(id: params[:id])
         erb :'/packages/show'
       else
         redirect to '/packages'
@@ -56,9 +53,8 @@ class PackagesController < ApplicationController
   end
 
   get '/packages/:id/edit' do
-    @package = Package.find(params[:id])
     if logged_in?
-      if @package.user == current_user
+      if @package = current_user.packages.find_by(id: params[:id])
         erb :'/packages/edit'
       else
         redirect to '/packages'
@@ -69,26 +65,32 @@ class PackagesController < ApplicationController
   end
 
   patch '/packages/:id' do
-    @package = Package.find(params[:id])
-    @package.update(params[:package])
+    if logged_in?
+      if @package = current_user.packages.find_by(id: params[:id])
+        @package.update(params[:package])
 
-    if params[:package][:shipping_co] == ""
-      @package.shipping_co = params[:alt_package][:shipping_co]
-    end
+        if params[:package][:shipping_co] == ""
+          @package.shipping_co = params[:alt_package][:shipping_co]
+        end
 
-    if params[:sender][:name].empty?
-      @package.sender = Sender.find_or_create_by(name: params[:alt_sender][:name].capitalize)
+        if params[:sender][:name].empty?
+          @package.sender = Sender.find_or_create_by(name: params[:alt_sender][:name].capitalize)
+        else
+          @package.sender = Sender.find_by(name: params[:sender][:name])
+        end
+        @package.save
+        redirect to "/packages/#{@package.id}"
+      else
+        redirect to '/'
+      end
     else
-      @package.sender = Sender.find_by(name: params[:sender][:name])
+      redirect to "/"
     end
-    @package.save
-    redirect to "/packages/#{@package.id}"
   end
 
   delete '/packages/:id/delete' do
     if logged_in?
-      package = Package.find(params[:id])
-      if package.user == current_user
+      if @package = current_user.packages.find_by(id: params[:id])
         package.destroy
         redirect to "/packages"
       else
